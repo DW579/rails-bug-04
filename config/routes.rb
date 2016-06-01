54053f360a8d2190cbe82cb7a18d07b51c5c8588
Dustin Wurtz <dtwurtz@gmail.com>
2016-05-31 19:00:44 -0700

    Found solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "task_lists#index"
-  get "signin" => "sessions#new", as: :signin
-  post "signin" => "sessions#create"
-  get "signout" => "sessions#destroy", as: :signout
+  get "/signin" => "sessions#new", as: :signin
+  post "/signin" => "sessions#create"
+  get "/signout" => "sessions#destroy", as: :signout
 
   resources :task_lists, only: [] do
     resources :tasks
